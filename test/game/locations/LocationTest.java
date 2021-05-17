ecdc833d78059d5e15f92603343de929b09ba0a6
u5490293@anu.edu.au <CYz19950123>
2021-05-17 23:17:00 +1000

    Meeting

--- test/game/locations/LocationTest.java
+++ test/game/locations/LocationTest.java
@@ -1,26 +1,27 @@
 package game.locations;
 
+import game.peons.Player;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LocationTest {
-
-
+    Player hero = new Player("cursedSoul");
+    Location map = new Location();
 
     @Test
     public void mainMap() {
+        map.mainMap(hero);
     }
 
     @Test
     public void battleMap() {
+        map.battleMap(hero);
     }
 
     @Test
     public void city() {
+        map.city(hero);
     }
 
-    @Test
-    public void bossRoom() {
-    }
 }
