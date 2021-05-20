c91d0b4bccc9c386d20fff89236f5c4d4db1896f
Yuwen  Wei <u6109932@anu.edu.au>
2021-05-20 14:59:40 +1000

    Update item part

--- src/game/GameLauncher.java
+++ src/game/GameLauncher.java
@@ -29,9 +29,9 @@ public class GameLauncher {
                 System.out.println();
                 switch (key) {
                     case '1':
-                        Player loadPlayer = loadPlayer();
-                        mainFlag = false;
-                        location.mainMap(loadPlayer);
+//                        Player loadPlayer = loadPlayer();
+//                        mainFlag = false;
+//                        location.mainMap(loadPlayer);
                         continue;
                     case '2':
                         System.out.println("Please type the name of the hero.");
@@ -72,38 +72,38 @@ public class GameLauncher {
 
     }
 
-    public static Player loadPlayer() {
-        Gson gson = new Gson();
-        JsonReader jsonReader;
-        try {
-            jsonReader = new JsonReader(new FileReader("json/profile/player.json"));
-            JsonObject player = gson.fromJson(jsonReader, JsonObject.class);
-            String[] tokens = player.toString().split(":");
-//            System.out.println(player);
-            int exp = removeLastX(tokens[1], 8);
-            int level  = removeLastX(tokens[2], 7);
-            String name = removeLastXS(tokens[3], 12);
-            name = name.replaceAll("\"","");
-//            System.out.println(name);
-            int healthMax = removeLastX(tokens[4], 9);
-            int health = removeLastX(tokens[5], 11);
-            int magicMax = removeLastX(tokens[6], 8);
-            int magic = removeLastX(tokens[7], 11);
-            int strength = removeLastX(tokens[8], 12);
-            int endurance = removeLastX(tokens[9], 10);
-            int agility = removeLastX(tokens[10], 7);
-            int luck = removeLastX(tokens[11], 15);
-            int intelligence = removeLastX(tokens[12], 9);
-            return new Player(name,exp,level,healthMax,health,magicMax,magic,strength,endurance,agility,luck,intelligence);
-        } catch (Exception e) {
-            System.err.println("There is an error when load: \"json/profile/player.json\"");
-            System.err.println("Generated new Player");
-            System.out.println("Please type the name of the hero.");
-            String str = readKeyBoard(10, false);
-            e.printStackTrace();
-            return new Player(str);
-        }
-    }
+//    public static Player loadPlayer() {
+//        Gson gson = new Gson();
+//        JsonReader jsonReader;
+//        try {
+//            jsonReader = new JsonReader(new FileReader("json/profile/player.json"));
+//            JsonObject player = gson.fromJson(jsonReader, JsonObject.class);
+//            String[] tokens = player.toString().split(":");
+////            System.out.println(player);
+//            int exp = removeLastX(tokens[1], 8);
+//            int level  = removeLastX(tokens[2], 7);
+//            String name = removeLastXS(tokens[3], 12);
+//            name = name.replaceAll("\"","");
+////            System.out.println(name);
+//            int healthMax = removeLastX(tokens[4], 9);
+//            int health = removeLastX(tokens[5], 11);
+//            int magicMax = removeLastX(tokens[6], 8);
+//            int magic = removeLastX(tokens[7], 11);
+//            int strength = removeLastX(tokens[8], 12);
+//            int endurance = removeLastX(tokens[9], 10);
+//            int agility = removeLastX(tokens[10], 7);
+//            int luck = removeLastX(tokens[11], 15);
+//            int intelligence = removeLastX(tokens[12], 9);
+//            return new Player(name,exp,level,healthMax,health,magicMax,magic,strength,endurance,agility,luck,intelligence);
+//        } catch (Exception e) {
+//            System.err.println("There is an error when load: \"json/profile/player.json\"");
+//            System.err.println("Generated new Player");
+//            System.out.println("Please type the name of the hero.");
+//            String str = readKeyBoard(10, false);
+//            e.printStackTrace();
+//            return new Player(str);
+//        }
+//    }
     private static int removeLastX(String str, int x) {
         if (str != null) {
             str = str.substring(0, str.length() - x);
